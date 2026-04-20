451e09f9ffc15e16d2b416e04e9cb07e3965d39d
rich <rickygb082002@gmail.com>
2026-04-19 20:44:35 -0600

    fix: repo delete sqllite

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# User (1) -> Pedido (n)
+class Pedido(models.Model):
+    customer_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return str(self.id)
